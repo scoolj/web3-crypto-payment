78b4b21489cd2cde74b7a3420adc64eb3e2af249
Scoolj <scoolj99@gmail.com>
2023-05-03 04:48:23 +0100

    web3 setup, navbar, welcome section

---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,38 @@
+import {AiFillPlayCircle} from 'react-icons/ai';
+import {SiEthereum} from 'react-icons/si';
+import {BsInfoCircle} from 'react-icons/bs';
+
+import  Loader  from './Loader';
+
+const Welcome =() => {
+    const commonStyles = 'min-h-[70px] sm:px-0 px-2 sm:win-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white';
+    const connectWallet = () => {
+
+    }
+    return(
+         <div className='flex w-full justify-center items-center'>
+            <div className='flex md:flex-row flex-col items-start justify-between md:20 py-12 px-4'>
+                <div className='flex flex-1 justify-start flex-col md-mr-10'>
+                    <h1 className='text-3xl sm:text-5xl text-white text-gradient py-1'>send crypto <br/>  across the world</h1>
+                    <p className='text-left mt-5 text-white font-light md:w-9/12  w-11/12 text-base'>
+                        Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
+                    </p>
+                    <button type='button' onClick={connectWallet} className='flex flex-row justify-center item-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]'>
+                        <p className='text-white text-base font-semibold'>Connect Wallet</p>
+                    </button>
+                    <div className=' grid sm:grid-cols-3 grid-col-2 w-full mt-10'>
+                        <div className={`rounded-tl-2xl ${commonStyles}`} > 
+                        
+                        </div> 
+                    </div>
+                </div>
+            </div>
+         </div>
+    );
+}
+
+
+export default Welcome;
+
+
+
